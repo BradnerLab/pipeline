9a1f67620733dea17c17f18d5639f04054b4a9f5
Charles Lin <charles.y.lin@gmail.com>
2013-09-30 14:54:59 -0400

    updated the pipeline_template to make it suck less

--- pipeline_template.py
+++ pipeline_template.py
@@ -38,15 +38,22 @@ metaFolder = '%smeta/' % (projectFolder)
 
 
 
-#LOADING THE DATA TABLE
-dataDict = pipeline_dfci.loadDataTable(dataFile)
-print(dataDict.keys())
 
 #==========================================================================
-#========================FORMATTING ANNOTATION=============================
+#========================FORMATTING SAMPLE TABLE===========================
 #==========================================================================
 
-#sampleTableFile = 'YOUR_WIGTC_ANNOTATION.xls'
+##THIS SECTION CREATES A DATA TABLE FROM A WHITEHEAD ANNOTATION SPREADSHEET
+
+##give full path
+##sampleTableFile = 'YOUR_WIGTC_ANNOTATION.xls' #<- the .xls file in the seq data folder provided by WI
+
+#dirpath = ''  <- provide full path of folder containing raw seq files
+##e.g. /ark/home/jr246/raw/130925_..../QualityScore/
+
+##bamPath <- where we store our bams.  Must have write access if you want to call bowtie
+##e.g. /ark/home/jr246/bam/
+#bamPath = '/ark/home/jr246/bam/'
 
 #pipeline_dfci.makePipelineTable(sampleTableFile,dirPath,bamPath,dataFile)
 
@@ -54,30 +61,59 @@ print(dataDict.keys())
 
 #namesList = dataDict.keys()
 
-print(namesList)
+#print(namesList)
+
+#==========================================================================
+#=======================LOADING DATA ANNOTATION============================
+#==========================================================================
+
+##THIS SECTION LOADS A DATA TABLE.  MUST BE UNCOMMENTED FOR REST OF CODE TO WORK
+
+
+#LOADING THE DATA TABLE
+dataDict = pipeline_dfci.loadDataTable(dataFile)
+print(dataDict.keys())
+
 
 
 #==========================================================================
 #==========================CALLING BOWTIE==================================
 #==========================================================================
 
-#namesList = []
+##THIS SECTION CALLS BOWTIE ON RAW READ FILES TO GENERATE SORTED AND INDEXED BAMS IN THE BAM FOLDER
 
+
+#namesList = []  <- fill this in if you want to only map a subset of the data. otherwise leave blank
+
+##SET LAUNCH TO False to debug
 #pipeline_dfci.makeBowtieBashJobs(dataFile,namesList,launch=True)
 
 #==========================================================================
 #=============================CALL MACS====================================
 #==========================================================================
 
+##THIS SECTION CALLS THE MACS ERROR MODEL
+
+
 #namesList = dataDict.keys()
 
 #print(namesList)
-
-
 #pipeline_dfci.callMacs(dataFile,macsFolder,namesList,overwrite=False,pvalue='1e-9')
 
 
+#==========================================================================
+#=======================FORMAT MACS OUTPUT=================================
+#==========================================================================
+
+##THIS SECTION FORMATS THE OUTPUT FROM MACS, CREATES THE MACSENRICHED FOLDER AND MOVES WIGGLES TO THE DESTINATION
+
 #pipeline_dfci.formatMacsOutput(dataFile,macsFolder,macsEnrichedFolder,wiggleFolder,wigLink='/ark/wiggles/')
+
+
+#==========================================================================
+#====================ADDITIONAL PIPELINE ANALYSIS==========================
+#==========================================================================
+
 
 
 
