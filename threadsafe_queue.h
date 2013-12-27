96999c99f4786c8f6956d2582b38d1e3c4ed207b
John DiMatteo <jdimatteo@gmail.com>
2013-12-27 18:24:36 -0500

    Removed functions wait_and_pop/try_pop functions that don't use shared_ptr, because I usually use this threadsafe queue in a producer/consumer pattern where the producer inserts nullptr to signal to the consumer to stop, and this pattern is only compatible with poping shared pointers

--- threadsafe_queue.h
+++ threadsafe_queue.h
@@ -47,23 +47,6 @@ public:
     threadsafe_queue()
     {}
 
-    void wait_and_pop(T& value)
-    {
-        std::unique_lock<std::mutex> lk(mut);
-        data_cond.wait(lk,[this]{return !data_queue.empty();});
-        value=std::move(*data_queue.front());
-        data_queue.pop();
-    }
-
-    bool try_pop(T& value)
-    {
-        std::lock_guard<std::mutex> lk(mut);
-        if(data_queue.empty())
-            return false;
-        value=std::move(*data_queue.front());
-        data_queue.pop();
-    }
-
     std::shared_ptr<T> wait_and_pop()
     {
         std::unique_lock<std::mutex> lk(mut);
