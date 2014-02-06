713b4ae75901c6e4d20ea989b9df33e075f4e7c1
John DiMatteo <jdimatteo@gmail.com>
2014-02-06 09:27:47 -0500

    Added email address next to any references to my name

--- bamliquidator_internal/bamliquidator_batch.m.cpp
+++ bamliquidator_internal/bamliquidator_batch.m.cpp
@@ -215,7 +215,7 @@ int main(int argc, char* argv[])
 
 /* The MIT License (MIT) 
 
-   Copyright (c) 2013 John DiMatteo 
+   Copyright (c) 2013 John DiMatteo (jdimatteo@gmail.com)
 
    Permission is hereby granted, free of charge, to any person obtaining a copy
    of this software and associated documentation files (the "Software"), to deal
--- bamliquidator_internal/normalize_plot_and_summarize.py
+++ bamliquidator_internal/normalize_plot_and_summarize.py
@@ -3,7 +3,7 @@
 ##################################################################################
 # The MIT License (MIT)
 #
-# Copyright (c) 2013 John DiMatteo 
+# Copyright (c) 2013 John DiMatteo (jdimatteo@gmail.com)
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
 # this software and associated documentation files (the "Software"), to deal in
