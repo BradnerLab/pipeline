eceee1f3a8ea8394965f2cdb6b7a3212fe2a0962
huihuifan <anarfish@gmail.com>
2014-07-16 15:34:42 -0400

    switching to embed

--- hockeysticks/js/frame.js
+++ hockeysticks/js/frame.js
@@ -9,12 +9,13 @@ this.screenshotPreview = function(filename){
        // these 2 variable determine popup's distance from the cursor
     console.log("here")
 
-    $("#linegraph").append("<p id='screenshot'><iframe width ='200px' height='250px' scrolling='no' src='"+ filename +"#page=1&toolbar=0&scrollbar=0&navpanes=0&statusbar=0' ></iframe>" +"</p>");                           
+
+    $("#linegraph").append("<p><embed src=" + filename + " type='application/pdf'></p>");                           
                                                                                    
-    $("#screenshot")
-        .css("top",(e.pageY - xOffset) + "px")
-        .css("left",(e.pageX + yOffset) + "px")
-        .fadeIn("fast");                                                                                                                       
+    // $("#screenshot")
+    //     .css("top",(e.pageY - xOffset) + "px")
+    //     .css("left",(e.pageX + yOffset) + "px")
+    //     .fadeIn("fast");                                                                                                                       
 };
  
  
