4ff938be3f2ccba14354c3f3fbf706f455558961
huihuifan <anarfish@gmail.com>
2014-07-16 16:03:26 -0400

    removed frame.js file, empty

--- hockeysticks/js/frame.js
+++
@@ -1,9 +0,0 @@
-
-screenshotPreview = function(filename){    
-
-    $("#linegraph").append("<p><embed src=" + filename + " scale='tofit' type='application/pdf' width='25%' height='60%' class='pdf_image'></p>");                           
-                                                                                   
-};
-
- 
- 
