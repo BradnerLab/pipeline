daa3576eaa1966e9f2f189e632229c143925fab8
huihuifan <anarfish@gmail.com>
2014-07-16 16:03:04 -0400

    moved preview function into main js file

--- hockeysticks/js/frame.js
+++ hockeysticks/js/frame.js
@@ -1,8 +1,5 @@
 
-
-
-
-this.screenshotPreview = function(filename){    
+screenshotPreview = function(filename){    
 
     $("#linegraph").append("<p><embed src=" + filename + " scale='tofit' type='application/pdf' width='25%' height='60%' class='pdf_image'></p>");                           
                                                                                    
--- hockeysticks/js/main.js
+++ hockeysticks/js/main.js
@@ -456,7 +456,7 @@ function update_linegraph(file) {
 						// 	console.log("here")
 						// 	this.attr("transform", "translate(" + d.rank + "0)");
 						// })
-						
+
 					}, delay);
 				}
 
@@ -730,4 +730,9 @@ $(document).ready(function () {
     });	
 });
 
+screenshotPreview = function(filename){    
+
+    $("#linegraph").append("<p><embed src=" + filename + " scale='tofit' type='application/pdf' width='25%' height='60%' class='pdf_image'></p>");                           
+                                                                                   
+};
 
