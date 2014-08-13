301cb7f1bb673be52a37fe91239c99534fe02e34
huihuifan <anarfish@gmail.com>
2014-08-12 22:24:25 -0400

    code for a bar chart added

--- hockeysticks/js/gene.js
+++ hockeysticks/js/gene.js
@@ -66,6 +66,63 @@ d3.csv("/Documents/Bradner_work/hockey-sticks/lookup_table.csv", function(error,
 
 			console.log(ranking_array)
 
+			var margin = {top: 20, right: 20, bottom: 30, left: 40},
+			    width = 960 - margin.left - margin.right,
+			    height = 300 - margin.top - margin.bottom;
+
+			var x = d3.scale.ordinal()
+			    .rangeRoundBands([0, width], .1);
+
+			var y = d3.scale.linear()
+			    .range([height, 0]);
+
+			var xAxis = d3.svg.axis()
+			    .scale(x)
+			    .orient("bottom");
+
+			var yAxis = d3.svg.axis()
+			    .scale(y)
+			    .orient("left")
+			    .ticks(10, "%");
+
+			var svg = d3.select("body").append("svg")
+			    .attr("width", width + margin.left + margin.right)
+			    .attr("height", height + margin.top + margin.bottom)
+			  .append("g")
+			    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+			x.domain([0, ranking_array.length]);
+			y.domain([0, d3.max(ranking_array, function(d) { return d.rank; })]);
+
+			svg.append("g")
+		      	.attr("class", "x axis")
+		      	.attr("transform", "translate(0," + height + ")")
+		      	.call(xAxis);
+
+			svg.append("g")
+		      	.attr("class", "y axis")
+		      	.call(yAxis)
+		    	.append("text")
+		      	.attr("transform", "rotate(-90)")
+		      	.attr("y", 6)
+		      	.attr("dy", ".71em")
+		      	.style("text-anchor", "end")
+		      	.text("Frequency");
+
+			svg.selectAll(".bar")
+			    .data(ranking_array)
+			    .enter().append("rect")
+			    .attr("class", "bar")
+			    .attr("x", function(d) { return x(d.filename); })
+			    .attr("width", x.rangeBand())
+			    .attr("y", function(d) { return y(d.frequency); })
+			    .attr("height", function(d) { return height - y(d.rank); });
+
+			function type(d) {
+			  d.frequency = +d.rank;
+			  return d;
+			}
+
 		})
 
 	}
