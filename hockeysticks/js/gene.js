d81c53c95a475a6b038ba2c37ed8bcd2443970e2
huihuifan <anarfish@gmail.com>
2014-08-14 14:14:08 -0400

    fixed line graph drawing multiple times

--- hockeysticks/js/gene.js
+++ hockeysticks/js/gene.js
@@ -39,9 +39,9 @@ d3.csv("/Documents/Bradner_work/hockey-sticks/lookup_table.csv", function(error,
 
 		//console.log(current_file)
 
-		plot(current_file)
+		plot(current_file, i)
 
-		function plot(current_file) {
+		function plot(current_file, number) {
 			d3.csv("/Documents/Bradner_work/hockey-sticks/" + current_file, function(error, current_data) {
 
 				// console.log(current_file)
@@ -87,112 +87,120 @@ d3.csv("/Documents/Bradner_work/hockey-sticks/lookup_table.csv", function(error,
 
 				//console.log(ranking_array)
 
-				tip = d3.tip().attr('class', 'd3-tip');
+				// console.log(number)
+				// console.log(file_name_array[0].length)
 
-				var margin = {top: 50, right: 50, bottom: 50, left: 100},
-				    width = 1200 - margin.left - margin.right,
-				    height = 300 - margin.top - margin.bottom;
+				if (number == 0) {
+					
+					tip = d3.tip().attr('class', 'd3-tip');
 
-				var x = d3.scale.ordinal()
-				    .rangeRoundBands([0, width], .1);
+					var margin = {top: 50, right: 50, bottom: 50, left: 100},
+					    width = 1200 - margin.left - margin.right,
+					    height = 300 - margin.top - margin.bottom;
 
-				var y = d3.scale.linear()
-				    .range([height, 0]);
+					var x = d3.scale.ordinal()
+					    .rangeRoundBands([0, width], .1);
 
-				var xAxis = d3.svg.axis()
-				    .scale(x)
-				    .orient("bottom")
-				    .tickFormat("");
+					var y = d3.scale.linear()
+					    .range([height, 0]);
 
-				var yAxis = d3.svg.axis()
-				    .scale(y)
-				    .orient("left")
-				    .outerTickSize([0]);
+					var xAxis = d3.svg.axis()
+					    .scale(x)
+					    .orient("bottom")
+					    .tickFormat("");
 
-				var svg = d3.select("body").append("svg")
-				    .attr("width", width + margin.left + margin.right)
-				    .attr("height", height + margin.top + margin.bottom)
-				  .append("g")
-				    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+					var yAxis = d3.svg.axis()
+					    .scale(y)
+					    .orient("left")
+					    .outerTickSize([0]);
 
-				svg.call(tip)
+					var svg = d3.select("body").append("svg")
+					    .attr("width", width + margin.left + margin.right)
+					    .attr("height", height + margin.top + margin.bottom)
+					  .append("g")
+					    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-				x.domain([0, ranking_array.length]);
-				y.domain([0, d3.max(ranking_array, function(d) { return d.rank; })]);
+					svg.call(tip)
 
-				svg.append("g")
-			      	.attr("class", "x axis")
-			      	.attr("transform", "translate(0," + height + ")")
-			      	.call(xAxis)
-			      	.append("text")
-			      	.attr("x", 1000)
-			      	.attr("y", 15)
-			      	.style("text-anchor", "middle")
-			      	.text("Files");
+					x.domain([0, ranking_array.length]);
+					y.domain([0, d3.max(ranking_array, function(d) { return d.rank; })]);
 
-				svg.append("g")
-			      	.attr("class", "y axis")
-			      	.call(yAxis)
-			    	.append("text")
-			      	.attr("transform", "rotate(-90)")
-			      	.attr("dy", "-3.71em")
-			      	.style("text-anchor", "end")
-			      	.text("Super-enhancer rank");
+					svg.append("g")
+				      	.attr("class", "x axis")
+				      	.attr("transform", "translate(0," + height + ")")
+				      	.call(xAxis)
+				      	.append("text")
+				      	.attr("x", 1000)
+				      	.attr("y", 15)
+				      	.style("text-anchor", "middle")
+				      	.text("Files");
 
-				svg.selectAll(".bar")
-				    .data(ranking_array)
-				    .enter().append("rect")
-				    .attr("class", "bar")
-				    .attr("x", function(d, i) {
-				    	return x(i)
-				    })
-				    .attr("width", x.rangeBand()-2)
-				    .attr("y", function(d) { return y(d.rank); })
-				    .attr("height", function(d) { return height - y(d.rank); })
-				    .on("mouseover", function(d) {
+					svg.append("g")
+				      	.attr("class", "y axis")
+				      	.call(yAxis)
+				    	.append("text")
+				      	.attr("transform", "rotate(-90)")
+				      	.attr("dy", "-3.71em")
+				      	.style("text-anchor", "end")
+				      	.text("Super-enhancer rank");
 
-				    	console.log(d)
+					svg.selectAll(".bar")
+					    .data(ranking_array)
+					    .enter().append("rect")
+					    .attr("class", "bar")
+					    .attr("x", function(d, i) {
+					    	return x(i)
+					    })
+					    .attr("width", x.rangeBand()-2)
+					    .attr("y", function(d) { return y(d.rank); })
+					    .attr("height", function(d) { return height - y(d.rank); })
+					    .on("mouseover", function(d) {
 
-				    	tip.html("File: " + d.filename + "<br>Rank: " + d.rank)
+					    	console.log(d)
 
-				    	return tip.show(d)
+					    	tip.html("File: " + d.filename + "<br>Rank: " + d.rank)
 
-				    })
-				    .on("mouseout", function(d) {
-				    	return tip.hide(d)
-				    });
+					    	return tip.show(d)
 
-				function type(d) {
-				  d.frequency = +d.rank;
-				  return d;
+					    })
+					    .on("mouseout", function(d) {
+					    	return tip.hide(d)
+					    });
+
+					function type(d) {
+					  d.frequency = +d.rank;
+					  return d;
+					}
+
+
+					d3.select("input#by_rank").on("change", change_rank);
+
+					function change_rank() {
+
+						// Copy-on-write since tweens are evaluated after a delay.
+						var x0 = x.domain(data.sort(this.checked
+						    ? function(a, b) { return b.rank - a.rank; }
+						    : function(a, b) { return d3.ascending(a.file, b.file); })
+						    .map(function(d) { return d.file; }))
+						    .copy();
+
+						var transition = svg.transition().duration(750),
+						    delay = function(d, i) { return i * 50; };
+
+						transition.selectAll(".bar")
+						    .delay(delay)
+						    .attr("x", function(d) { return x0(d.file); });
+
+						transition.select(".x.axis")
+						    .call(xAxis)
+						  .selectAll("g")
+						    .delay(delay);
+
+					}
+
 				}
 
-
-				d3.select("input#by_rank").on("change", change_rank);
-
-				function change_rank() {
-
-					// Copy-on-write since tweens are evaluated after a delay.
-					var x0 = x.domain(data.sort(this.checked
-					    ? function(a, b) { return b.rank - a.rank; }
-					    : function(a, b) { return d3.ascending(a.file, b.file); })
-					    .map(function(d) { return d.file; }))
-					    .copy();
-
-					var transition = svg.transition().duration(750),
-					    delay = function(d, i) { return i * 50; };
-
-					transition.selectAll(".bar")
-					    .delay(delay)
-					    .attr("x", function(d) { return x0(d.file); });
-
-					transition.select(".x.axis")
-					    .call(xAxis)
-					  .selectAll("g")
-					    .delay(delay);
-
-				}
-
+				
 
 
 			})
