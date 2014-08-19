ef1cb409b6470a319d8ea8801cf43f18b87f874c
John DiMatteo <jdimatteo@gmail.com>
2014-08-18 20:59:07 -0600

    moved git calls out of makefile

---
+++ bamliquidator_internal/make_deb.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# this script is intended to be run by the makefile in this directory
+# it used to be part of the makefile, but it resulted in git being a dependency of running "make"
+# which was not permitted by the ppa build
+
+git diff --quiet
+if [ $? -eq 0 ]; then
+CLEAN=clean
+else
+CLEAN=unclean
+fi
+
+CHANGELOG="bamliquidator ($VERSION-0ppa1~%s) %s; urgency=low
+
+  * Auto generated from makefile
+  * $(git config --get remote.origin.url)
+  * $(git rev-parse HEAD) ($CLEAN) 
+
+ -- $UPLOADER <$UPLOADER_EMAIL>  $(date +"%a, %d %b %G %H:%M:%S %z")
+
+"
+
+mv bamliquidator-$VERSION.tar.gz bamliquidator_$VERSION.orig.tar.gz 
+tar xf bamliquidator_$VERSION.orig.tar.gz
+cp -R debian bamliquidator-$VERSION
+cp -R bamliquidator-$VERSION bamliquidator-$VERSION-precise
+mv bamliquidator-$VERSION bamliquidator-$VERSION-trusty
+printf "$CHANGELOG" precise precise > bamliquidator-$VERSION-precise/debian/changelog 
+printf "$CHANGELOG" trusty trusty > bamliquidator-$VERSION-trusty/debian/changelog 
+cd bamliquidator-$VERSION-precise/debian; debuild -S
+cd bamliquidator-$VERSION-trusty/debian; debuild -S
