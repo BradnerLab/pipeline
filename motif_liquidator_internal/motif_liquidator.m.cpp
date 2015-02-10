d6a5fc950bb00921b2ed2570d30249e9f182ff1d
John DiMatteo <jdimatteo@gmail.com>
2015-02-09 20:28:20 -0700

    Basic searching implemented, just counting:
    $ ./motif_liquidator ../../copied_from_tod/20110819_580_hg19.sorted.bam "TGGGAA"
    g++ motif_liquidator.m.cpp -O3 -g -Wall -march=native -o motif_liquidator -lbam -lz -lpthread -ltcmalloc_minimal -ltbb
    match count: 1897376
    
    real  1m38.936s
    user  1m34.312s
    sys 0m3.159s

--- motif_liquidator_internal/motif_liquidator.m.cpp
+++ motif_liquidator_internal/motif_liquidator.m.cpp
@@ -3,19 +3,37 @@
 
 void test(const std::string& bam_file, const std::string& target)
 {
-  bam1_t* read = bam_init1(); // what is this for?
+  bam1_t* read = bam_init1();
   bamFile file_handle = bam_open(bam_file.c_str(), "r");
   bam_header_t* header = bam_header_read(file_handle);
 
   if (file_handle == 0 || read == 0 || header == 0) throw std::runtime_error("failed to open " + bam_file);
 
+  std::string sequence(50, ' '); // todo: use the actual read length 
+
   size_t count = 0;
-  std::cout << "Hello?" << std::endl;
-  while ( bam_read1(file_handle, read) >= 0 )
+  while (bam_read1(file_handle, read) >= 0)
   {
-    ++count;
+    const bam1_core_t *c = &read->core;
+    uint8_t *s = bam1_seq(read);
+
+    // [s, s+c->l_qseq) is the sequence, with two bases packed into each byte
+    // I bet we could directly search that instead of first storing as a string
+    // but lets get something simple working first. An intermediate step could be
+    // to search integers without using bam_nt16_rev_table (and I wouldn't have
+    // to worry about the packing complexity).
+
+    for (int i = 0; i < c->l_qseq; ++i)
+    {
+      sequence[i] = bam_nt16_rev_table[bam1_seqi(s, i)];
+    }
+
+    if (sequence.find(target) != std::string::npos)
+    {
+      ++count;
+    }
   }
-  std::cout << "count: " << count << std::endl;
+  std::cout << "match count: " << count << std::endl;
 
   bam_header_destroy(header);
   bam_close(file_handle);
