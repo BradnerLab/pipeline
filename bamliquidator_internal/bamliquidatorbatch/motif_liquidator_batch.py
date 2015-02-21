c7f89b2b2eeb5c2b320fad3afb3eb59760acb48e
John DiMatteo <jdimatteo@gmail.com>
2015-02-20 21:12:49 -0700

    Updated arg parsing, preparing to calculate region ratios

--- bamliquidator_internal/bamliquidatorbatch/motif_liquidator_batch.py
+++ bamliquidator_internal/bamliquidatorbatch/motif_liquidator_batch.py
@@ -16,18 +16,18 @@ from flattener import write_tab_for_all
 
 __version__ = util.version
 
-def create_ratio_table(h5file):
+def create_bin_ratio_table(h5file):
     class Ratio(tables.IsDescription):
         bin_number = tables.UInt32Col(pos=0)
         chromosome = tables.StringCol(util.chromosome_name_length, pos=1)
         ratio      = tables.Float64Col(pos=2)
 
-    table = h5file.create_table('/', 'ratios', Ratio, 'filtered count / baseline count for each bin')
+    table = h5file.create_table('/', 'bin_ratios', Ratio, 'filtered count / baseline count for each bin')
     table.flush()
 
     return table
 
-def populate_ratios(baseline_counts, baseline_file_key, filtered_counts, filtered_file_key, ratios):
+def populate_bin_ratios(baseline_counts, baseline_file_key, filtered_counts, filtered_file_key, ratios):
     for baseline_row, filtered_row in itertools.izip_longest(baseline_counts.where('file_key == %d' % baseline_file_key),
                                                              filtered_counts.where('file_key == %d' % filtered_file_key)):
         if baseline_row is None or filtered_row is None:
@@ -52,28 +52,34 @@ def populate_ratios(baseline_counts, baseline_file_key, filtered_counts, filtere
     sorted_ratios.flush()
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='Calculates bin or region ratios between the motif matching read counts and '
+                                     'the total read counts.  May use results from prior runs of %s, bamliquidator_batch, or '
+                                     'motif_liquidator to skip regenerating intermediate results.' % sys.argv[0])
 
-    parser.add_argument('motif')
+    parser.add_argument('bam_file', nargs='?', help='The bam file to search for the motif for and run bamliquidator on. '
+                                                    'If not provided, then both --prior_filtered_bam and --baseline args '
+                                                    'must be provided.')
 
-    parser.add_argument('bam_file')
+    motif_or_filtered_grp = parser.add_mutually_exclusive_group(required=True)
+    motif_or_filtered_grp.add_argument('-m', '--motif', help='The short string to search for in reads in the bam, e.g. "TGGGAA".  '
+                                              'If not provided, then a previously filtered bam file must be provided '
+                                              'with the --prior_filtered_bam arg.')
+    motif_or_filtered_grp.add_argument('-p', '--prior_filtered_bam',
+                                       help='A bam already filtered by the motif, e.g. from a prior %s or motif_liquidator '
+                                            'run.' % sys.argv[0])
 
+    bin_or_region_grp = parser.add_mutually_exclusive_group()
     default_bin_size = 5000
-    parser.add_argument('-b', '--bin_size', type=int, default=default_bin_size,
-                        help='Number of base pairs in each bin -- the smaller the bin size the longer the runtime and '
-                             'the larger the data files (default is %d)' % default_bin_size)
+    bin_or_region_grp.add_argument('-b', '--bin_size', type=int, default=default_bin_size,
+                                   help='Number of base pairs in each bin -- the smaller the bin size the longer the runtime and '
+                                        'the larger the data files (default is %d).  Not compatible with --regions_file '
+                                        'argument.' % default_bin_size)
+    bin_or_region_grp.add_argument('-r', '--regions_file',
+                                   help='Region file in either .gff or .bed format.  Not compatible with --bin_size argument.')
 
-    # todo: consider replacing -b arg with a --pass_through arg, which would require usage of unsafe shell=True, e.g.
-    #default_pass_through_args='-b 5000 --skip_plot'
-    #parser.add_argument('--pass_through', default=default_pass_through_args,
-    #                    help='Override arguments to pass through to bamliquidator_batch.py. Should not include '
-    #                         '-o/--ouput nor positional input .bam arguments. '
-    #                         'Default is "%s".' % default_pass_through_args)
-
-    parser.add_argument('--baseline', help='Baseline counts.h5 file from bamliquidator_batch.py. Bin size should '
-                                           'match bin size in --pass_through argument.  If not provided, then '
-                                           'it is generated and stored in output directory.')
-
+    parser.add_argument('--baseline', help='Baseline counts.h5 file from prior %s or bamliquidator_batch.py run. Bin size or '
+                                           'regions should match the --bins or --regions_file argument.  If not provided, then '
+                                           'it is generated and stored in output directory.' % sys.argv[0])
     default_output_directory='./output'
     parser.add_argument('-o', '--output_directory', default=default_output_directory,
                         help='Directory to store results (and intermediate files). Creates directory if necessary. '
@@ -88,6 +94,10 @@ def main():
     parser.add_argument('--version', action='version', version='%s %s' % (os.path.basename(sys.argv[0]), __version__))
 
     args = parser.parse_args()
+
+    if args.baseline is None and args.bam_file is None:
+        logging.error('Either the --baseline arg or the bam_file positional arg must be provided.')
+        sys.exit(1)
     
     assert(tables.__version__ >= '3.0.0')
  
@@ -96,42 +106,43 @@ def main():
     util.configure_logging(args, args.output_directory, quiet=False)
     # todo, log file doesn't contain child process logging
 
-    logging.info('Running motif_liquidator to get filtered bam')
-    start = time()
-    motif_executable = util.most_appropriate_executable_path('motif_liquidator')
-    filtered_bam_path = os.path.join(args.output_directory, 'filtered.' + os.path.basename(args.bam_file))
-    subprocess.check_call([motif_executable, args.bam_file, args.motif, filtered_bam_path])
-    duration = time() - start
-    logging.info('Running motif_liquidator took %f seconds' % duration)
+    if args.prior_filtered_bam:
+        filtered_bam_path = args.prior_filtered_bam
+    else:
+        if args.bam_file is None or args.motif is None:
+            logging.error('Unless --prior_filtered_bam arg is provided, both the --motif arg and the bam_file positional arg are required.')
+            sys.exit(1)
+        logging.info('Running motif_liquidator to get filtered bam')
+        start = time()
+        motif_executable = util.most_appropriate_executable_path('motif_liquidator')
+        filtered_bam_path = os.path.join(args.output_directory, 'filtered.' + os.path.basename(args.bam_file))
+        subprocess.check_call([motif_executable, args.bam_file, args.motif, filtered_bam_path])
+        duration = time() - start
+        logging.info('Running motif_liquidator took %f seconds' % duration)
+
+    bamliquidator_batch_args = [os.path.join(os.path.dirname(os.path.realpath(__file__)), 'bamliquidator_batch.py'),
+                                '--skip_plot',
+                                '--bin_size=%d' % args.bin_size if args.bin_size else '--regions_file=%s' % args.regions_file]
 
     logging.info('Running bamliquidator_batch on filtered bam')
     start = time()
-    bamliquidator_batch_executable = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'bamliquidator_batch.py')
     filtered_bamliquidator_output_path = os.path.join(args.output_directory, 'filtered_bamliquidator_output')
-    subprocess.check_call([bamliquidator_batch_executable,
-                           '--bin_size=%d' % args.bin_size,
-                           '--skip_plot',
-                           '--output_directory=%s' % filtered_bamliquidator_output_path,
-                           filtered_bam_path])
+    subprocess.check_call(bamliquidator_batch_args + ['--output_directory=%s' % filtered_bamliquidator_output_path, filtered_bam_path])
     filtered_counts_file_path = os.path.join(filtered_bamliquidator_output_path, 'counts.h5')
     filtered_file_key = 1
     duration = time() - start
     logging.info('Running bamliquidator_batch on filtered bam took %f seconds' % duration)
 
-    if args.baseline is None:
+    if args.baseline:
+        baseline_counts_file_path = args.baseline
+    else:
         logging.info('Running bamliquidator_batch on (unfiltered) bam')
         start = time()
         baseline_bamliquidator_output_path = os.path.join(args.output_directory, 'baseline_bamliquidator_output')
-        subprocess.check_call([bamliquidator_batch_executable,
-                               '--bin_size=%d' % args.bin_size,
-                               '--skip_plot',
-                               '--output_directory=%s' % baseline_bamliquidator_output_path,
-                               args.bam_file])
+        subprocess.check_call(bamliquidator_batch_args + ['--output_directory=%s' % baseline_bamliquidator_output_path, args.bam_file])
         baseline_counts_file_path = os.path.join(baseline_bamliquidator_output_path, 'counts.h5')
         duration = time() - start
         logging.info('Running bamliquidator_batch on (unfiltered) bam took %f seconds' % duration)
-    else:
-        baseline_counts_file_path = args.baseline
     baseline_file_key = 1 # todo: support args.baseline with file_key not equal to 1
 
     ratio_file_path = os.path.join(args.output_directory, 'ratios.h5')
@@ -139,14 +150,24 @@ def main():
     with tables.open_file(baseline_counts_file_path, mode = 'r') as baseline_counts_h5:
         with tables.open_file(filtered_counts_file_path, mode = 'r') as filtered_counts_h5:
             with tables.open_file(ratio_file_path, 'w') as ratios_h5:
-                ratios = create_ratio_table(ratios_h5)
-
-                baseline_counts = baseline_counts_h5.root.bin_counts
-                filtered_counts = filtered_counts_h5.root.bin_counts
-
                 logging.info('Calculating ratios between filtered and unfiltered bin counts')
                 start = time()
-                populate_ratios(baseline_counts, baseline_file_key, filtered_counts, filtered_file_key, ratios)
+                if args.bin_size:
+                    ratios = create_bin_ratio_table(ratios_h5)
+
+                    baseline_counts = baseline_counts_h5.root.bin_counts
+                    filtered_counts = filtered_counts_h5.root.bin_counts
+
+                    populate_bin_ratios(baseline_counts, baseline_file_key, filtered_counts, filtered_file_key, ratios)
+                else:
+                    ratios = create_region_ratio_table(ratios_h5)
+
+                    baseline_counts = baseline_counts_h5.root.bin_counts
+                    filtered_counts = filtered_counts_h5.root.bin_counts
+
+                    populate_region_ratios(baseline_counts, baseline_file_key, filtered_counts, filtered_file_key, ratios)
+                    
+                    
                 duration = time() - start
                 logging.info('Calculating ratios took %f seconds' % duration)
 
