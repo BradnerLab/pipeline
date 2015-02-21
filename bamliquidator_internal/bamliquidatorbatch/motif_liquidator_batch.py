616ddf375aea3e3b9f4a9b39ec336a2b5fb6fa2e
John DiMatteo <jdimatteo@gmail.com>
2015-02-20 21:58:50 -0700

    Region motif ratios seems to be working now
    time ./motif_liquidator_batch.py -p motif_liquidator_batch_output_before_regions/filtered.20110819_580_hg19.sorted.bam -r ../../../copied_from_tod/HG19_SUM159_BRD4_-0_+0.gff --baseline bamliquidator_batch_small_region_output/counts.h5 -f
    Running bamliquidator_batch on filtered bam
    Liquidating motif_liquidator_batch_output_before_regions/filtered.20110819_580_hg19.sorted.bam (file 1 of 1)
    Liquidation completed: 0.350443 seconds
    Normalizing
    Post liquidation processing took 0.010496 seconds
    Flattening HDF5 tables into text files
    Flattening took 0.018514 seconds
    Running bamliquidator_batch on filtered bam took 1.706708 seconds
    Calculating ratios between filtered and unfiltered bin counts
    Calculating ratios took 0.029689 seconds
    Flattening ratios tables
    Flattening took 0.011781 seconds
    
    real  0m2.025s
    user  0m1.589s
    sys 0m0.347s

--- bamliquidator_internal/bamliquidatorbatch/motif_liquidator_batch.py
+++ bamliquidator_internal/bamliquidatorbatch/motif_liquidator_batch.py
@@ -18,11 +18,25 @@ __version__ = util.version
 
 def create_bin_ratio_table(h5file):
     class Ratio(tables.IsDescription):
-        bin_number = tables.UInt32Col(pos=0)
-        chromosome = tables.StringCol(util.chromosome_name_length, pos=1)
+        chromosome = tables.StringCol(util.chromosome_name_length, pos=0)
+        bin_number = tables.UInt32Col(pos=1)
         ratio      = tables.Float64Col(pos=2)
 
     table = h5file.create_table('/', 'bin_ratios', Ratio, 'filtered count / baseline count for each bin')
+    table.flush()
+
+    return table
+
+def create_region_ratio_table(h5file):
+    class Ratio(tables.IsDescription):
+        chromosome  = tables.StringCol(util.chromosome_name_length, pos=0)
+        region_name = tables.StringCol(64, pos=1)
+        start       = tables.UInt64Col(pos=2)
+        stop        = tables.UInt64Col(pos=3)
+        strand      = tables.StringCol(1, pos=4)
+        ratio       = tables.Float64Col(pos=5)
+
+    table = h5file.create_table('/', 'region_ratios', Ratio, 'filtered count / baseline count for each bin')
     table.flush()
 
     return table
@@ -33,11 +47,11 @@ def populate_bin_ratios(baseline_counts, baseline_file_key, filtered_counts, fil
         if baseline_row is None or filtered_row is None:
             raise RuntimeError('mismatched number of bin counts between baseline and filtered bams -- were inconsistent bin sizes used?')
 
-        assert(baseline_row['bin_number'] == filtered_row['bin_number'])
         assert(baseline_row['chromosome'] == filtered_row['chromosome'])
+        assert(baseline_row['bin_number'] == filtered_row['bin_number'])
 
-        ratios.row['bin_number'] = baseline_row['bin_number']
         ratios.row['chromosome'] = baseline_row['chromosome']
+        ratios.row['bin_number'] = baseline_row['bin_number']
         ratios.row['ratio'] = 0 if baseline_row['count'] == 0 else filtered_row['count'] / baseline_row['count']
         ratios.row.append()
 
@@ -46,7 +60,35 @@ def populate_bin_ratios(baseline_counts, baseline_file_key, filtered_counts, fil
     ratios.cols.chromosome.create_csindex()
     ratios.cols.ratio.create_csindex()
 
-    sorted_ratios = ratios.copy(newname='sorted_ratios', sortby=ratios.cols.ratio,
+    sorted_ratios = ratios.copy(newname='sorted_bin_ratios', sortby=ratios.cols.ratio,
+                                step=-1, checkCSI=True,
+                                title='ratio table sorted in decreasing ratio column order')
+    sorted_ratios.flush()
+
+def populate_region_ratios(baseline_counts, baseline_file_key, filtered_counts, filtered_file_key, ratios):
+    for baseline_row, filtered_row in itertools.izip_longest(baseline_counts.where('file_key == %d' % baseline_file_key),
+                                                             filtered_counts.where('file_key == %d' % filtered_file_key)):
+        if baseline_row is None or filtered_row is None:
+            raise RuntimeError('mismatched number of region counts between baseline and filtered bams -- were inconsistent regions used?')
+
+        assert(baseline_row['chromosome'] == filtered_row['chromosome'])
+        assert(baseline_row['start'] == filtered_row['start'])
+        assert(baseline_row['stop'] == filtered_row['stop'])
+        assert(baseline_row['strand'] == filtered_row['strand'])
+
+        ratios.row['chromosome'] = baseline_row['chromosome']
+        ratios.row['region_name'] = baseline_row['region_name']
+        ratios.row['start'] = baseline_row['start']
+        ratios.row['stop'] = baseline_row['stop']
+        ratios.row['strand'] = baseline_row['strand']
+        ratios.row['ratio'] = 0 if baseline_row['count'] == 0 else filtered_row['count'] / baseline_row['count']
+        ratios.row.append()
+
+    ratios.flush()
+    # if anybody actually queries the hdf5 files directly, they are welcome to add additional indices here
+    ratios.cols.ratio.create_csindex()
+
+    sorted_ratios = ratios.copy(newname='sorted_region_ratios', sortby=ratios.cols.ratio,
                                 step=-1, checkCSI=True,
                                 title='ratio table sorted in decreasing ratio column order')
     sorted_ratios.flush()
@@ -76,6 +118,12 @@ def main():
                                         'argument.' % default_bin_size)
     bin_or_region_grp.add_argument('-r', '--regions_file',
                                    help='Region file in either .gff or .bed format.  Not compatible with --bin_size argument.')
+
+    parser.add_argument('-e', '--extension', type=int, default=0,
+                        help='Extends reads by n bp (default is 0)')
+    parser.add_argument('--sense', default=None, choices=['+', '-', '.'],
+                        help="Map to '+' (forward), '-' (reverse) or '.' (both) strands. For gff regions, default is to use "
+                             "the sense specified by the gff file; otherwise, default maps to both.")
 
     parser.add_argument('--baseline', help='Baseline counts.h5 file from prior %s or bamliquidator_batch.py run. Bin size or '
                                            'regions should match the --bins or --regions_file argument.  If not provided, then '
@@ -122,7 +170,12 @@ def main():
 
     bamliquidator_batch_args = [os.path.join(os.path.dirname(os.path.realpath(__file__)), 'bamliquidator_batch.py'),
                                 '--skip_plot',
-                                '--bin_size=%d' % args.bin_size if args.bin_size else '--regions_file=%s' % args.regions_file]
+                                '--regions_file=%s' % args.regions_file if args.regions_file else '--bin_size=%d' % args.bin_size,
+                                '--extension=%s' % args.extension]
+    if args.sense:
+        bamliquidator_batch_args.append('--sense=%s' % args.sense)
+    if args.flatten:
+        bamliquidator_batch_args.append('--flatten')
 
     logging.info('Running bamliquidator_batch on filtered bam')
     start = time()
@@ -152,21 +205,20 @@ def main():
             with tables.open_file(ratio_file_path, 'w') as ratios_h5:
                 logging.info('Calculating ratios between filtered and unfiltered bin counts')
                 start = time()
-                if args.bin_size:
+                if args.regions_file:
+                    ratios = create_region_ratio_table(ratios_h5)
+
+                    baseline_counts = baseline_counts_h5.root.region_counts
+                    filtered_counts = filtered_counts_h5.root.region_counts
+
+                    populate_region_ratios(baseline_counts, baseline_file_key, filtered_counts, filtered_file_key, ratios)
+                else:
                     ratios = create_bin_ratio_table(ratios_h5)
 
                     baseline_counts = baseline_counts_h5.root.bin_counts
                     filtered_counts = filtered_counts_h5.root.bin_counts
 
                     populate_bin_ratios(baseline_counts, baseline_file_key, filtered_counts, filtered_file_key, ratios)
-                else:
-                    ratios = create_region_ratio_table(ratios_h5)
-
-                    baseline_counts = baseline_counts_h5.root.bin_counts
-                    filtered_counts = filtered_counts_h5.root.bin_counts
-
-                    populate_region_ratios(baseline_counts, baseline_file_key, filtered_counts, filtered_file_key, ratios)
-                    
                     
                 duration = time() - start
                 logging.info('Calculating ratios took %f seconds' % duration)
