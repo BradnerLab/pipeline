fa143860af98cd751715a6f31df25ef95eb8ae78
John DiMatteo <jdimatteo@gmail.com>
2015-03-02 23:17:45 -0700

    Added some basic tests

--- bamliquidator_internal/test.cpp
+++ bamliquidator_internal/test.cpp
@@ -1,13 +1,31 @@
 #include "gtest/gtest.h"
 
-TEST(initial, passing)
-{
-  ASSERT_TRUE(true);
-}
+#include "bamliquidator_util.h"
 
-TEST(initial, failing)
+TEST(contains, misc)
 {
-  ASSERT_TRUE(false);
+  EXPECT_FALSE(contains("", ""));
+  EXPECT_FALSE(contains("", "A"));
+  EXPECT_FALSE(contains("A", "B"));
+
+  EXPECT_TRUE(contains("A", "A"));
+  EXPECT_TRUE(contains("abc", "a"));
+  EXPECT_TRUE(contains("abc", "bc"));
+  EXPECT_TRUE(contains("bcbcbcbcdbcbc", "bcd"));
+  EXPECT_FALSE(contains("bcbcbcbcdbcbc", "bccd"));
+  EXPECT_FALSE(contains("abc", "d"));
+
+  EXPECT_TRUE(contains("N", "N"));
+  EXPECT_TRUE(contains("N", "A"));
+  EXPECT_TRUE(contains("A", "N"));
+
+  EXPECT_TRUE(contains("AB", "N"));
+  EXPECT_TRUE(contains("NB", "N"));
+  EXPECT_TRUE(contains("BN", "N"));
+  
+  EXPECT_FALSE(contains("BB", "AN"));
+  EXPECT_FALSE(contains("BN", "AN"));
+  EXPECT_TRUE(contains("ABB", "ANB"));
 }
 
 int main(int argc, char **argv)
