91eb6c4a7793d0d87eec85ce0789688ca73370ba
John DiMatteo <jdimatteo@gmail.com>
2015-09-13 15:46:52 -0600

    Implemented but didn't yet test basic fasta parsing

--- liquidator/fasta_reader.h
+++ liquidator/fasta_reader.h
@@ -23,10 +23,19 @@ FastaReader::FastaReader(std::istream &fasta_file)
 
 bool FastaReader::next_read(std::string &sequence, std::string &sequence_name, char& strand)
 {
+    // this function assumes that every sequence is preceded by a ">" line, with no empty lines allowed.
+    // this is an approximation of the fasta format.
     if (!m_fasta_file)
     {
         throw std::runtime_error("can't read next sequence because istream is not good");
     }
+    std::getline(m_fasta_file, sequence_name);
+    if (!m_fasta_file || sequence_name.empty() || sequence_name[0] != '>')
+    {
+        throw std::runtime_error("fasta sequence description is missing");
+    }
+    sequence_name.erase(0); // remove the '>' from the name
+    std::getline(m_fasta_file, sequence);
     return m_fasta_file;
 }
 
