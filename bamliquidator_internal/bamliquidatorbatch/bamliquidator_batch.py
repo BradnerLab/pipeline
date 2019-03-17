15e34ab0c7c3357e357de7b90dbc6fde5c5a51cd
John DiMatteo <jdimatteo@boulderlabs.com>
2019-03-17 17:57:14 -0600

    support bionic for ppa

--- bamliquidator_internal/bamliquidatorbatch/bamliquidator_batch.py
+++ bamliquidator_internal/bamliquidatorbatch/bamliquidator_batch.py
@@ -20,7 +20,7 @@ from time import time
 from os.path import basename
 from os.path import dirname
 
-__version__ = '1.3.6'
+__version__ = '1.3.8'
 
 default_black_list = ["chrUn", "_random", "Zv9_", "_hap"]
 
